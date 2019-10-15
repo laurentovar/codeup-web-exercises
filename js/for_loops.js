fbbd2ef2e2231a052cd1107549ac386db2ac55fe
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-15 11:34:43 -0500

    cone do-while loop

---
+++ js/for_loops.js
@@ -0,0 +1,18 @@
+(function () {
+//function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number
+    //multiply by the numbers 1 - 10
+    function showMultiplicationTable(input) {
+        
+    }
+
+
+//use for loop and other code to generate 10 random numbers between 20 and 200 and output to the console
+//if each number is odd or even
+
+
+//create a for loop that uses console.log to create the output shown (1,22,333,4444,55555,666666,to 9)
+
+//create a for loop that uses console.log to create the output shown below (100,95,90,85,80,75 to 5)
+
+
+})();
--- js/while.js
+++ js/while.js
@@ -1,3 +1,4 @@
+(function () {
 //a number times 2(2,4, 8,16,32,64,128,256,512...) create a while loop that uses console.log
 var i= 1;
 while(i < 65536) {
@@ -11,9 +12,15 @@ while(i < 65536) {
     // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
+var conesWanted= Math.floor(Math.random() * 5) + 1;
 
 //output should be similar to:
     //5 cones sold.. there are enough cones (if enough cones)
     //cannot sell you 6 cones if I only have 3 (if not enough cones)
     //yay. I sold all the cones (if no more cones)
+    do {
+        //body
+
+    } while ();
+
+})();
