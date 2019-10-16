45af250d88db71615b3e5a1c18705261d7694de2
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-15 22:02:20 -0500

    loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -10,5 +10,25 @@ for (var g= 1 ; g <= 49; g++){
 
     }
 }
+
+
+
+for (var i = 1 ; i <= 49; i++) {
+
+    if (i % 2 == 0) {
+        continue;
+    } else if (i == 27) {
+        console.log("Yikes! Skipping number " + i);
+        continue;
+    } else {
+        console.log("here is the odd number " + i);
+    }
+
+}
+
+
+
+
+
 })();
 
