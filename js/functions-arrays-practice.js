98d37abfb65ef90f13994a5faf2c788764f18b24
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-18 15:54:53 -0500

    functions/arrays practice

--- js/functions-arrays-practice.js
+++ js/functions-arrays-practice.js
@@ -16,6 +16,12 @@ function allIndexOf(arg1, arg2) {
 //function named removeAll(array, value) that takes in two arguments.
 // The first argument should be an array and the second argument should be a value you wish to remove
 var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+    //remove ant
+    // bugs.splice(0, 2);
+console.log(bugs);
+//remove mosquito
+
+    //remove roach
 
 
 //=======//
@@ -25,6 +31,7 @@ function first(x) {
     x === food;
     return x[0];
 }
+
 //function n named getLongestString that takes in an array of strings
 // and returns the longest string of that array
 var longest = ["cat", "puppy", "cows" ,"snakes" , "doggggooooo"];
@@ -54,8 +61,46 @@ function getShortestString(arr) {
 }
 console.log(getShortestString(shortest));
 
+
+//function called coinFlip that returns either 0 or 1 randomly
+function coinFlip() {
+    return (Math.floor(Math.random()*2));
+}
+
+console.log(coinFlip());
+
+
+//      you can make it specifically say heads or tails
+function flipCoin() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+}
+console.log(flipCoin());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //  Write a function named getRandomQuote().
 //   Inside of the function, create an array of strings where each string is a quote or thought you find inspirational
 //   getRandomQuote should generate a random number between 0 and the array's length minus 1
 //   use the randomly generated number as your index
 //   return a random quote.
+    //random quotes:
+    // "A successful person is one who takes these challenges and emerges out victorious. -Unknown"
+    // "Insanity: doing the same thing over and over again and expecting different results -Einstein"
+    // "When you go in search of honey you must expect to be stung by bees -Joseph Joubert"
+    // "Nothing is hopeless; we must hope for everything. -Euripides"
+    // "To live is so startling it leaves little time for anything else. -Emily Dickinson"
+    // "Love looks not with the eyes, but with the mind; And therefore is winged Cupid painted blind.-William Shakespeare"
+    // "The price of anything is the amount of life you exchange for it -Henry David Thoreau"
+
