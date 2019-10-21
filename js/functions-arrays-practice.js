6983e4db40abaf58a1818b617bec207cb9ada650
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-20 21:50:22 -0500

    removeAll function

--- js/functions-arrays-practice.js
+++ js/functions-arrays-practice.js
@@ -16,15 +16,35 @@ function allIndexOf(arg1, arg2) {
 //function named removeAll(array, value) that takes in two arguments.
 // The first argument should be an array and the second argument should be a value you wish to remove
 var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
     //remove ant
-    // bugs.splice(0, 2);
-console.log(bugs);
-//remove mosquito
+function removeAll (array, remove) {
+    for (var i =  0; i < array.length -1; i++){
+        console.log(array);
+        if(array.indexOf(remove) != -1){
+            array.splice(array.indexOf(remove), 1);
+        }
+
+        //if (array [i] === remove){
+            //console.log("Found a " + remove)
+            //delete array[i];
+            //array.splice(i,1);
+        //}
+    }
+    return array;
+}
+console.log("START Remove All Ant");
+console.log(removeAll(bugs, "ant"));
+
+    //remove mosquito
+console.log("START Remove All Mosquito");
+console.log(removeAll(bugs, "mosquito"));
 
     //remove roach
+console.log("START Remove All Roach");
+console.log(removeAll(bugs, "roach"));
 
 
-//=======//
 // Write a function named first() that returns only the first element of an array
 var food = ["popcorn", "pizza", "hamburgers", "muffins"];
 function first(x) {
@@ -90,9 +110,6 @@ console.log(flipCoin());
 //   use the randomly generated number as your index
 //   return a random quote.
     //random quotes:
-
-
-
 function getRandomQuote() {
     var quotes = ["Insanity: doing the same thing over and over again and expecting different results -Einstein",
      "Nothing is hopeless; we must hope for everything. -Euripides" ,
