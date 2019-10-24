54a88650c9d291263656338b49a4b837cfefe9cf
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-24 09:22:12 -0500

    morning warmup

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -172,3 +172,21 @@ function createPersonObject(name, age) {
 }
 
 console.log(createPersonObject("Hannah", 27));
+
+//write fizzbuzz. divisable by 3 fizz, divisable by 5 buzz, divisable by both fizzbuzz
+function fizzBuzz(num) {
+    for (var i= 1; i <= num; i++){
+        if (i % 5 === 0 && i % 3 === 0){
+            console.log("FizzBuzz");
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+        else if (i % 3 === 0){
+            console.log("Fizz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
