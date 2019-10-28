4938f010eee4ec6d0aa20bdbb3ea2c913738ab19
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-28 09:19:31 -0500

    morning warmup

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -201,3 +201,42 @@ function returnFive(num) {
 }
 
 console.log(returnFive("Five"));
+
+//function that returns the shopping cart object with the most oranges
+var shoppingCarts = [
+    {
+        apples: 8,
+        bananas: 2,
+        oranges: 4,
+        grapes: 24
+    }, {
+        apples: 3,
+        bananas: 6,
+        oranges: 8,
+        grapes: 12
+    } , {
+        apples: 0,
+        bananas: 12,
+        oranges: 0,
+        grapes: 0
+    } , {
+        apples: 4,
+        bananas: 0,
+        oranges: 12,
+        grapes: 10
+    }
+];
+function mostOranges(carts) {
+    var winner = carts[0];
+    carts.forEach(function (cart) {
+        if (cart.oranges > winner.oranges){
+            winner = cart;
+        }
+    });
+    return winner;
+}
+
+console.log(mostOranges(shoppingCarts));
+
+
+
