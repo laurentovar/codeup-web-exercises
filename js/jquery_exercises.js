aa8fa6f79327dab88c637512390264eadab188af
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-29 12:06:01 -0500

    jquery mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,27 +3,49 @@
 $(document).ready(function() {
     // alert( 'The DOM has finished loading!' );
 
+
+ //for the selectors exercise
     // var contents = $("#flower-type-list").html();
     // alert(contents);
 
     //this alerts what the contents of about us are
-    var content = $("#about-us").html();
-    alert(content);
+    // var content = $("#about-us").html();
+    // alert(content);
+    //
+    // //create a border around all elements with the class codeup that is 1px wide, red
+    // $('.codeup').css('border', '1px solid red');
+    //
+    // //set the font-size of all li elements to 20px
+    // $('li').css('font-size', '20px');
+    //
+    // //alert the contents of your h1 elements
+    // var contents = $('h1').html();
+    // alert(contents);
+    //
+    // //craft selectors that highlight all h1, p, li elements
+    // $('h1, p, li').css('background-color', '#61FFFF');
 
-    //create a border around all elements with the class codeup that is 1px wide, red
-    $('.codeup').css('border', '1px solid red');
+ //for the mouse events exercise
 
-    //set the font-size of all li elements to 20px
-    $('li').css('font-size', '20px');
+    //change the background color of an h1 element when clicked
+    $('h1').click(function () {
+        $('h1').css('background-color','#418591');
+    });
 
-    //alert the contents of your h1 elements
-    var contents = $('h1').html();
-    alert(contents);
+    //make all paragraphs have a font-size of 18px when doubleclicked
+    $('p').dblclick(function () {
+        $('p').css('font-size','18px');
+    });
 
-    //craft selectors that highlight all h1, p, li elements
-    $('h1, p, li').css('background-color', '#61FFFF');
-
-
+    //set all li text color to red when the mouse is hovering, reset to black when its not
+    $('li').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        }
+    );
 
 
 
