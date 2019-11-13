1ce4a589e42a1106689ebeda1f9da63a47a07e4b
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-13 16:45:53 -0600

    promises updated exercise

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,20 @@
 //Write a function named wait that accepts a number as a parameter,
 // and returns a promise that resolves after the passed number of milliseconds.
 
-function wait (num){
+//the way to write it in a function
+const wait = num => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, num);
+    });
+};
 
-}
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+
+//this is how you can write them apart
 let promise1 = new Promise(function(resolve, reject) {
     setTimeout(function() {
         resolve('You\'ll see this after 1 second');
@@ -28,12 +36,18 @@ promise2.then(function(value) {
 console.log(promise2);
 
 
+
 //Create a function that accepts a GitHub username, and returns a promise
 // that resolves with the date of the last commit that user made.
 // Reference the github api documentation to achieve this.
 
-// GET /repos/:owner/:repo/git/commits/:sha
-let githubPromise = new Promise(function (resolve, reject) {
-    fetch(url, {headers: {'Authorization': github}});
+const lastCommit = (username) => {
+    fetch(`https://api.github.com/users/${username}/events`, {headers: {"Authorization": `token ${github}`}})
+        .then(resp => resp.json())
+        .then(data => data.find(event => event.type === "PushEvent"))
+        .then(event => event.created_at)
+        .then(date => console.log(date));
+};
 
-});
+lastCommit("laurentovar");
+
