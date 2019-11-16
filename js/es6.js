2c8a63c7ff21d02f972f601fd65340d4ea087470
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-16 12:27:19 -0600

    updated es6

--- js/es6.js
+++ js/es6.js
@@ -54,50 +54,63 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
+//Originals
+  // users.forEach(function(user) {
+  //   return emails.push(user.email);
+  // });
+  //
+  // users.forEach(function(user) {
+  //   return names.push(user.name);
+  // });
+
+//ES6 Way
+users.forEach((user) => {
   return emails.push(user.email);
 });
+console.log(emails);
 
-
-
-users.forEach(function(user) {
+users.forEach((user) => {
   return names.push(user.name);
 });
+console.log(names);
+
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
+users.forEach(function({name, email,languages}) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
-
-  const [a,b,c,d, e]= users;
-  const emails = [a[1]];
-  const demiosInstructors = [c,d];
-
-  // console.log(europaInstructors);
-  console.log(demiosInstructors);
+  // const name = user.name;
+  // const email = user.email;
+  // const languages = user.languages;
 
 
   // TODO: rewrite the assignment below to use template strings
   // developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));\
-  developers.push(`${name} email is ${email} ${name} knows ${languages.join(' ,')}`);
+  developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(' ,')}`);
 
 });
+
+console.log(developers);
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
-  for (let developer of users){
-    console.log(developer);
-  }
 
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-    });
+//Old Way
+// developers.forEach(function (developer) {
+//
+//   // TODO: rewrite the assignment below to use template strings
+//   list += '<li>' + developer + '</li>';
+// });
+// list += '</ul>';
+
+//es6 way
+for (let developer of developers){
+  list += `<li>  ${developer} </li>`;
+}
 list += '</ul>';
+
+$("#practice").append(list);
